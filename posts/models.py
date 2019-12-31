1c4d6f95c831a13734c41b3f9e8afe39902562d4
Mike King <nyntofive@gmail.com>
2019-12-30 21:14:59 -0500

    create/update posts, Styled list view/detail view

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.shortcuts import reverse
 
 
 class User(AbstractUser):
@@ -16,9 +17,25 @@ class Post(models.Model):
     publish_date = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    slug = models.SlugField()
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("detail", kwargs={"slug": self.slug})
+
+    @property
+    def get_comment_count(self):
+        return self.comment_set.all().count()
+
+    @property
+    def get_view_count(self):
+        return self.postview_set.all().count()
+
+    @property
+    def get_like_count(self):
+        return self.like_set.all().count()
 
 
 class Comment(models.Model):
