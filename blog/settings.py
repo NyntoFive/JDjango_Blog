1a4d832f32b83b15d86e98dce84d85e1b6c6fbfb
Mike <nyntofive@gmail.com>
2020-01-05 00:23:03 +0000

    .

--- blog/settings.py
+++ blog/settings.py
@@ -130,8 +130,8 @@ USE_TZ = True
 STATIC_URL = "/static/"
 MEDIA_URL = "/media/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static_root/")
-STATICFILES_DIRS = ["static"]
+STATICFILES_DIRS = ["static/","static_root/"]
+STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media_root")
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
